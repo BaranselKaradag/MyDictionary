85e51558cb2c6eee85416e114b0cac4fa704a200
Baransel Karadağ <Br.09@outlook.com>
2021-03-04 21:21:25 +0200

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+
+        TValue[] values;
+        public int count { get; set; }
+
+
+
+
+
+        public MyDictionary()
+        {
+            keys = new TKey[1];
+            values = new TValue[1];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] tempKey = new TKey[keys.Length];
+            TValue[] tempValue = new TValue[values.Length];
+            tempKey = keys;
+            tempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+        }
+
+
+        public void Show(TKey key)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                if(Convert.ToString(i)==Convert.ToString(key))
+                {
+                  count=0;
+                  count=i;
+                }
+            }
+            if (count!=0)
+            {
+                Console.WriteLine(keys[count]+ ": "+values[count]);
+            }
+
+            else
+            {
+                Console.WriteLine("Değer Mevcut Değil.........");
+            }
+        }
+
+
+        public void Listele()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+
+                Console.WriteLine(keys[i]+ " "+ values[i]);
+
+            }
+
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            do
+            {
+
+                {
+                    Console.WriteLine("Ekle:1  Veri Bulma :2    Veri Listele:3  Exit:4    ");
+                    string secim = Console.ReadLine();
+                    if (secim == 1.ToString())
+                    {
+                        Console.WriteLine("Key No :");
+                        int keyNo = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Değer Girişi :");
+                        string value = Console.ReadLine();
+                        myDictionary.Add(keyNo, value);
+                    }
+                    else if (secim == 2.ToString())
+                    {
+                        Console.WriteLine("Key No :");
+                        string keyNo = Console.ReadLine();
+                        myDictionary.Show(Convert.ToInt32(keyNo));
+
+                    }
+
+                    else if (secim == 3.ToString())
+                    {
+                        myDictionary.Listele();
+                    }
+
+                }
+
+                Console.WriteLine("Ana Ekran : 1   Exit: 4");
+
+            }
+            while (Console.ReadLine() != 4.ToString());
+
+
+        }
+    }
+}
